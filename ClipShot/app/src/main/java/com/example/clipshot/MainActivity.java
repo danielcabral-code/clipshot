7cddf8bf427420659cb1a55faba721da30665cf1
danielcabral-code <1danielcabral@gmail.com>
2020-05-07 12:19:20 +0100

    add cores e actionbar

--- ClipShot/app/src/main/java/com/example/clipshot/MainActivity.java
+++ ClipShot/app/src/main/java/com/example/clipshot/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.clipshot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
+import android.os.Build;
 import android.os.Bundle;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("WrongConstant")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Objects.requireNonNull(getSupportActionBar()).setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.action_bar_layout);
     }
 }
