3ffba828f3c0b62220bdadabf4fdec02c1487c81
danielcabral-code <1danielcabral@gmail.com>
2020-05-09 16:31:05 +0100

    functional navbar and topbar

---
+++ ClipShot/app/src/main/java/com/example/clipshot/FeedFragment.java
@@ -0,0 +1,33 @@
+package com.example.clipshot;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FeedFragment extends Fragment {
+
+    public FeedFragment() {
+        // Required empty public constructor
+    }
+
+    public static FeedFragment newInstance(String param1, String param2) {
+        FeedFragment fragment = new FeedFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_feed, container, false);
+    }
+}
--- ClipShot/app/src/main/java/com/example/clipshot/MainActivity.java
+++ ClipShot/app/src/main/java/com/example/clipshot/MainActivity.java
@@ -1,26 +1,27 @@
 package com.example.clipshot;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
-import android.transition.Fade;
-import android.transition.Transition;
 import android.view.View;
+import android.widget.RelativeLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.bottomappbar.BottomAppBar;
 
 import java.util.Objects;
 
-public class ProfileActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @SuppressLint("WrongConstant")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.activity_main);
 
         // Call TopBar
         Objects.requireNonNull(getSupportActionBar()).setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
@@ -30,11 +31,26 @@ public class ProfileActivity extends AppCompatActivity {
         getSupportActionBar().setElevation(50f); // Float == px
         BottomAppBar bottomAppBar = findViewById(R.id.navigation);
         bottomAppBar.setElevation(50f); // Float == px
+
+        // Opens Feed page by default
+        openFragment(FeedFragment.newInstance("",""));
     }
 
     // Go To Feed (NavBar Button)
     public void goToFeed(View v) {
-        Intent accessFeed = new Intent(this, FeedActivity.class);
-        startActivity(accessFeed);
+        openFragment(FeedFragment.newInstance("",""));
+    }
+
+    // Go To Profile (NavBar Button)
+    public void goToProfile(View v) {
+        openFragment(ProfileFragment.newInstance("",""));
+    }
+
+    // Utilitary method for opening fragments
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.navBar, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
