7bdd41f78d5879e05657603a2f78f096dba46462
danielcabral-code <1danielcabral@gmail.com>
2020-05-22 19:06:03 +0100

    feed search bar added

--- ClipShot/app/src/main/java/com/example/clipshot/FeedFragment.java
+++ ClipShot/app/src/main/java/com/example/clipshot/FeedFragment.java
@@ -2,13 +2,23 @@ package com.example.clipshot;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageView;
 import androidx.fragment.app.Fragment;
 
+import android.text.Layout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.Objects;
+
 public class FeedFragment extends Fragment {
+
+    private AppCompatImageView iconSearch;
+    private int SEARCHBAR_VISIBILITY = 0;
 
     public FeedFragment() {
         // Required empty public constructor
@@ -29,5 +39,32 @@ public class FeedFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_feed, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        iconSearch = Objects.requireNonNull(getActivity()).findViewById(R.id.iconSearch);
+
+        Log.d("checkClick", String.valueOf(SEARCHBAR_VISIBILITY));
+
+        iconSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                View searchBarContainer = Objects.requireNonNull(getActivity()).findViewById(R.id.searchBarContainer);
+
+                if (SEARCHBAR_VISIBILITY == 1) {
+                    searchBarContainer.setVisibility(View.INVISIBLE);
+                    Objects.requireNonNull(((AppCompatActivity) getActivity()).getSupportActionBar()).setElevation(20f); // Float == px
+                    SEARCHBAR_VISIBILITY = 0;
+                } else {
+                    searchBarContainer.setVisibility(View.VISIBLE);
+                    Objects.requireNonNull(((AppCompatActivity) getActivity()).getSupportActionBar()).setElevation(0f); // Float == px
+                    SEARCHBAR_VISIBILITY = 1;
+                }
+            }
+        });
     }
 }
